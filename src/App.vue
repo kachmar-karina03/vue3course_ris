c9dfdbf9a8d26123375851fc42ce0893d8f42417
JuliaKovtun <yuliia.kovtun@gotoinc.co>
2023-07-03 18:29:28 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="app">
+    <post-form
+      @create="createPost"
+    />
+    <post-list
+      :posts="posts"
+    />
+  </div>
+</template>
+
+
+<script>
+import PostForm from "@/components/PostForm";
+import PostList from "@/components/PostList";
+export default {
+  components: {
+    PostList, PostForm
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript 1', body: 'Post description. 1'},
+        {id: 2, title: 'JavaScript 2', body: 'Post description. 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Post description. 3'},
+        {id: 4, title: 'JavaScript 4', body: 'Post description. 4'}
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post);
+    }
+  }
+}
+</script>
+
+
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  .app {
+    padding: 20px;
+  }
+
+</style>
---
+++ src/components/PostList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h3>List of users:</h3>
+    <post-item
+      v-for="post in posts"
+      :post="post"
+    />
+
+  </div>
+</template>
+
+<script>
+import PostItem from '@/components/PostItem.vue';
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
