e5ea7ed7a727e5ee976f3b5202d2cffeef561f9d
JuliaKovtun <yuliia.kovtun@gotoinc.co>
2023-08-28 14:49:30 +0300

    Added search and pagination

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app">
     <h1>Posts</h1>
+    <my-input
+      v-model="searchQuery"
+      placeholder="Search..."
+    />
     <div class="app__btns">
       <my-button
         @click="showDialog"
@@ -19,11 +23,24 @@
       />
     </my-dialog>
     <post-list
-      :posts="posts"
+      :posts="sortedAndSearchedPosts"
       @remove="removePost"
       v-if="!isPostsLoading"
     />
     <div v-else>Loading...</div>
+    <div class="page__wrapper">
+      <div 
+        v-for="pageNumber in totalPages"
+        :key="page"
+        class="page"
+        :class="{
+          'current-page': page === pageNumber
+        }"
+        @click="changePage(pageNumber)"
+      >
+        {{ pageNumber }}
+      </div>
+    </div>
   </div>
 </template>
 
@@ -43,6 +60,10 @@ export default {
       dialogVisible: false,
       isPostsLoading: false,
       selectedSort: '',
+      searchQuery: '',
+      page: 1,
+      limit: 7,
+      totalPages: 0,
       sortOptions: [
         { value: 'title', name: 'By title' },
         { value: 'body', name: 'By body' },
@@ -60,10 +81,20 @@ export default {
     showDialog(){
       this.dialogVisible = true;
     },
+    changePage(pageNumber){
+      this.page = pageNumber
+      // this.fetchPosts()
+    },
     async fetchPosts() {
       try {
         this.isPostsLoading = true;
-        const response = await axios.get('https://jsonplaceholder.typicode.com/posts?_limit=10');
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+          params: {
+            _page: this.page,
+            _limit: this.limit
+          }
+        });
+        this.totalPages = Math.ceil(response.headers['x-total-count'] / this.limit);
         this.posts = response.data;
       } catch(e) {
         alert('Error!');
@@ -76,6 +107,21 @@ export default {
   },
   mounted() {
     this.fetchPosts();
+  },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2) => {
+        return post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+      })
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter(post => post.title.toLowerCase().includes(this.searchQuery.toLowerCase()))
+    }
+  },
+  watch: {
+    page() {
+      this.fetchPosts()
+    }
   }
 }
 </script>
@@ -98,4 +144,17 @@ export default {
     margin: 15px 0;
   }
 
+  .page__wrapper {
+    display: flex;
+    margin-top: 15px;
+  }
+
+  .page {
+    border: 1px solid black;
+    padding: 10px;
+  }
+
+  .current-page {
+    border: 2px solid teal;
+  }
 </style>
